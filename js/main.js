1ac1d54ace16e9bf93d017ce2edd34ed6ab72b94
Kyle Kettler <hello@kylekettler.com>
2023-05-30 17:01:20 +0000

    Define a function named renderEntry with a single parameter named entry. This parameter represents a single object for a single entry from the data.entries array. The function should generate and return a DOM tree for a single entry that matches the entries you created in the unordered list. #2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const $entryImg = document.querySelector('#entry-img');
+const $newEntryImg = document.querySelector('#entry-img');
 const $imgInput = document.querySelector('#img-url');
 const $newEntryForm = document.querySelector('#new-entry-form');
 
 $imgInput.addEventListener('input', event => {
   const imgPath = event.target.value;
-  $entryImg.src = imgPath;
+  $newEntryImg.src = imgPath;
 });
 
 $newEntryForm.addEventListener('submit', event => {
@@ -16,6 +16,38 @@ $newEntryForm.addEventListener('submit', event => {
   newEntry.entryID = data.nextEntryId;
   data.nextEntryId++;
   data.entries.unshift(newEntry);
-  $entryImg.src = 'images/placeholder-image-square.jpg';
+  $newEntryImg.src = 'images/placeholder-image-square.jpg';
   $newEntryForm.reset();
 });
+
+function renderEntry(entry) {
+  const $entryItem = document.createElement('li');
+  $entryItem.setAttribute('class', 'row');
+
+  const $entryCol1 = document.createElement('div');
+  $entryCol1.setAttribute('class', 'col-half');
+
+  const $entryImg = document.createElement('img');
+  $entryImg.setAttribute('src', entry.photoURL);
+  $entryImg.setAttribute('class', 'entry-img');
+  $entryImg.setAttribute('alt', entry.title);
+
+  const $entryCol2 = document.createElement('div');
+  $entryCol2.setAttribute('class', 'col-half row');
+
+  const $entryH2 = document.createElement('h2');
+  $entryH2.textContent = entry.title;
+
+  const $entryNote = document.createElement('p');
+  $entryNote.textContent = entry.notes;
+
+  $entryItem.appendChild($entryCol1);
+  $entryItem.appendChild($entryCol2);
+  $entryCol1.appendChild($entryImg);
+  $entryCol2.appendChild($entryH2);
+  $entryCol2.appendChild($entryNote);
+
+  return $entryItem;
+}
+
+renderEntry(data.entries[0]);
